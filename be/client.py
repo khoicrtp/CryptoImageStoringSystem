f3a057b353d15609294d4181b1172c89c3472056
zhangbaoyi <63915636+zhangbaoyi@users.noreply.github.com>
2022-01-09 05:10:13 +0700

    Update client.py

--- be/client.py
+++ be/client.py
@@ -5,5 +5,5 @@ import json
 BASE="http://127.0.0.1:5000/"
 json_data = json.dumps({'username':'suir2', 'password': 'a', 'confirm_password': 'a', 'publickey': '638'})
 
-response = requests.post(BASE + "users/login/", json=json_data)
-print(response)
+response = requests.get(BASE + "suir2/images_list/", json=json_data)
+print(response, response.json())
