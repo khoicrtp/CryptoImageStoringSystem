605268f9312f505cfb103a7fecf4641cf88251de
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-01-09 08:29:59 +0700

    add controller of auth

---
+++ demo/controller/auth_controller.py
@@ -0,0 +1,20 @@
+import requests
+BASE="http://localhost:5000"
+
+def login_verify(user,pw):
+    json_data = {'username':user,'password':pw}
+    response = requests.post(BASE+'/users/login',data=json_data)
+    if(response.status_code==500):
+        return True,response.json()
+    elif(response.status_code==404):
+        return False, None
+    
+def login_verify(user,pw,n,e):
+    json_data = {'username':user,'password':pw,'n_publickey':n,'e_publickey':e}
+    response = requests.post(BASE+'/users/register',data=json_data)
+    if(response.status_code==500):
+        return True
+    elif(response.status_code==404):
+        return False
+    
+    
