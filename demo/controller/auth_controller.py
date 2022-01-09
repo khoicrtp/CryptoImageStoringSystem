d7a4c07e3c60f621f29156eb8dc3e548cdf7591c
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-01-09 08:44:01 +0700

    update login and register function

--- demo/controller/auth_controller.py
+++ demo/controller/auth_controller.py
@@ -9,7 +9,7 @@ def login_verify(user,pw):
     elif(response.status_code==404):
         return False, None
     
-def login_verify(user,pw,n,e):
+def register_user(user,pw,n,e):
     json_data = {'username':user,'password':pw,'n_publickey':n,'e_publickey':e}
     response = requests.post(BASE+'/users/register',data=json_data)
     if(response.status_code==500):
