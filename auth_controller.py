2118da78fd68b98c77938d4b71f6f3c49cb7a464
zhangbaoyi <63915636+zhangbaoyi@users.noreply.github.com>
2022-01-10 16:41:18 +0700

    login wwAuth done

--- auth_controller.py
+++ auth_controller.py
@@ -1,18 +1,21 @@
 import requests
+import json
+
 BASE="https://test-eflask-crypto.herokuapp.com/"
 
 def login_verify(user,pw):
-    json_data = {'username':user,'password':pw}
-    response = requests.post(BASE+'/users/login',data=json_data)
-    if(response.status_code==500):
-        return True,response.json()
+    json_data = json.dumps({'username':user,'password':pw})
+    response = requests.post(BASE+'/users/login',json=json_data)
+    if(response.status_code==200):
+        # print(response.json())
+        return True#, None #,response.json()
     elif(response.status_code==404):
         return False, None
     
 def register_user(user,pw,n,e):
-    json_data = {'username':user,'password':pw,'n_publickey':n,'e_publickey':e}
-    response = requests.post(BASE+'/users/register',data=json_data)
-    if(response.status_code==500):
+    json_data = json.dumps({'username':user,'password':pw,'n_publickey':n,'e_publickey':e})
+    response = requests.post(BASE+'/users/register',json=json_data)
+    if(response.status_code==200):
         return True
     elif(response.status_code==404):
         return False
